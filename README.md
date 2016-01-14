4e039a6f0888c8fe7152466efc254e7915701fed
Frank Cheng <ufo22940268@gmail.com>
2016-01-14 15:09:28 +0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+#Jenkins bitbar
+
+A [bitbar](https://github.com/matryer/bitbar#get-started) plugin to display jenkins status.
+
+![](./screenshot.png)
+
+## How to use
+
+1. Download bitbar from [here](https://github.com/matryer/bitbar/releases).
+2. Set bitbar folder to `script`.
+3. Set the [BASE_URL](./script/jenkins-multiple-projects-status.1m.sh) to your jenkins website and the project your want to observe. 
+4. Then it will fetch the task status per minutes and update the icon in your task menu.
+
---
+++ script/jenkins-multiple-projects-status.1m.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+BASE_URL="jenkins.xinpinget.com/"
+PROJECTS=("Saturn")
+
+function displaytime {
+  local T=$1/1000
+  local D=$((T/60/60/24))
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  local output=""
+
+  if [[ $D -gt 0  || $H -gt 0 || $M -gt 10 ]]
+  then
+    output+=">10mn"
+  else
+    output+="${M}mn ${S}s"
+  fi
+
+  echo "${output} ago"
+}
+
+# beginning of display
+# echo "Jenkins Status"
+# echo "---"
+
+for project in "${PROJECTS[@]}"
+do
+  output="${project}: "
+  url="http://${BASE_URL}/job/${project}/lastBuild/api/json?pretty=true"
+  query=$(curl --insecure --silent "${url}") # take only the end of output
+
+  success=$(echo "${query}" | grep "result" | awk '{print $3}') # grep the "result" line
+  if [[ $success == *"SUCCESS"* ]]
+  then
+    output+='🔵 '
+  else
+    output+='🔴 '
+  fi
+
+  timestamp=$(echo "${query}" | grep "timestamp" | awk '{print $3}') # grep the "timestamp" line
+  timestamp=${timestamp%?} # remove the trailing ','
+  currentTime=$(($(date +'%s * 1000 + %-N / 1000000'))) # generate a timestamp
+  output+=" $(displaytime $(( currentTime - timestamp )))"
+  echo "${output}"
+done
